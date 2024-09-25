4fa89e6ec74972193dd59934443a15ed3aaff765
ATOM3448 <Qweru2003@gmail.com>
2024-09-25 10:28:28 +0700

    Some fix first_automat

--- Семестр 1/Теория языков программирования и методы трансляции/first_automat/src/main/java/plt/samples/LanguageHandler.java
+++ Семестр 1/Теория языков программирования и методы трансляции/first_automat/src/main/java/plt/samples/LanguageHandler.java
@@ -2,14 +2,11 @@ package plt.samples;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 /**
  * Класс определяющий взаимодействие значений с заданным языком
